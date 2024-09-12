eff3ab628c18293d5a2cd61e0e4d9c84583a9bb7
Lamiya Alom <lamiyaalomlamu@gmail.com>
2024-09-12 20:46:34 +0600

    Add files via upload

---
+++ p (3).py
@@ -0,0 +1,8 @@
+def triangle_area():
+    base = float(input("base : "))
+    height = float(input("height: "))
+    area = 0.5 * base * height
+    print(f"The area of the triangle is {area} square units.")
+triangle_area()
+
+
